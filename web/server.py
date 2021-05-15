abbd319f42299056ae9f1fb1169b2aec1e4c7bd9
zelosz <79079495+zeloszb@users.noreply.github.com>
2021-05-15 11:23:38 +0200

    Server

--- web/server.py
+++ web/server.py
@@ -1,1 +1,13 @@
+from flask import Flask, render_template
+
+#Initalize
+app = Flask(__name__, static_folder="./web/static")
+
+@app.route("/")
+def hello():
+    return render_template("./web/Flood-It-Web.html")
+
+#Run the server localy
+if __name__ == '__main__':
+    app.run(host="localhost", port=5000)
 
